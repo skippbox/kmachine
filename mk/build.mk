899c560df8bab14eec02e8c53adb27bcbaf07d5d
Sebastien Goasguen <runseb@gmail.com>
2015-12-14 17:48:38 +0100

    Fix cross build with new kmachine binary name

--- mk/build.mk
+++ mk/build.mk
@@ -6,7 +6,7 @@ extension = $(patsubst windows,.exe,$(filter windows,$(1)))
 # Cross builder helper
 define gocross
 	GOOS=$(1) GOARCH=$(2) CGO_ENABLED=0 go build \
-		-o $(PREFIX)/bin/docker-machine_$(1)-$(2)/docker-$(patsubst cmd/%.go,%,$3)$(call extension,$(GOOS)) \
+		-o $(PREFIX)/bin/kmachine_$(1)-$(2)/k$(patsubst cmd/%.go,%,$3)$(call extension,$(GOOS)) \
 		-a $(VERBOSE_GO) -tags "static_build netgo $(BUILDTAGS)" -installsuffix netgo \
 		-ldflags "$(GO_LDFLAGS) -extldflags -static" $(GO_GCFLAGS) $(3);
 endef
